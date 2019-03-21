21a5e024a7ba47a8b22be69a8b6f0eff4e571e2e
1564321656426820132 <nt77265226763csm@gmail.com>
2019-03-22 02:30:44 +0900

    訂正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -31,9 +31,40 @@ public class Test : MonoBehaviour {
     void Start () {
 
 
+
+
+
+        //課題
+
+        //要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
+        int[] Array = { 0, 1, 2, 3, 4 };
+
+        //for文を使い、配列の各要素の値を順番に表示
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(Array[i]);
+        }
+
+
+
+        //for文を使い、配列の各要素の値を逆順に表示
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            Debug.Log(Array[4 - i]);
+        }
+
+
+        //課題は以上。
+
+
+        //発展課題
+
         //Magic関数を呼び出して、魔法を使用
         //Magic関数を10回使った後に、更にMagic関数を呼び出すと、mpが足りないメッセージが表示されることを確認
-        
+
 
 
         Boss lastboss = new Boss();
@@ -41,6 +72,9 @@ public class Test : MonoBehaviour {
         {
             lastboss.magic();
         }
+
+        //発展課題は以上。
+
     }
 	
 	// Update is called once per frame
